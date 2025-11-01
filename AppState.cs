70aedd9620d884d898ce468cb588cafd4bf6d948
Your Name <you@example.com>
2025-11-01 18:12:16 +0000

    Initial commit

---
+++ AppState.cs
@@ -0,0 +1,13 @@
+﻿namespace ImcApp.Models;
+//para armazenar os outputs no browser, esta é uma das formas
+public static class AppState
+{
+    public static List<ImacModel> History { get; set; } = new();
+    
+    public static event EventHandler<EventArgs> OnChanged;
+
+    public static void NotifyChanged(object? sender, EventArgs e)
+    {
+        OnChanged.Invoke(sender, e);
+    }
+}
---
+++ Models/ImacModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImcApp.Models;
+
+public class ImacModel
+{
+    [Required]
+    [Range(1,3)]
+    public double? Height { get; set; }
+
+    [Required]
+    [Range(15,180)]
+    public double? Weight { get; set; }
+
+    public DateTime CreatedAt { get; set; }= DateTime.Now;
+
+    public double Imc => (Weight ?? 1) / ((Height ?? 1) * (Height ?? 1)) ;
+}
